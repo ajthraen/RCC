8e674126c895ed32bcdb10d619b636fb567bbb98
AJ <ajthraen@gmail.com>
2022-09-11 13:57:13 -0400

    react setup 1

---
+++ my-app/src/Components/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div className="container">
+            <div className='navBar'>
+                <div className="flexBox1">
+                    Top Dog Training
+                </div>
+                <div className="flexBox2">
+                    Home
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Nav;
