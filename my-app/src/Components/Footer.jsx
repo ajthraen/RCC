a841da5e452b6805a63a3a59848b5ae4d4ff2ae2
AJ <ajthraen@gmail.com>
2022-09-11 19:17:29 -0400

    react CC

---
+++ my-app/src/Components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="container2">
+            <div className='navBar'>
+                <div className="flexBox1">
+                    Copyright 2022 Top Dog Training LLC
+                </div>
+                <div className="flexBox2">
+                    Contact Us
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
